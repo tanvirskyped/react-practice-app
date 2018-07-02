949b3fcd54cb6e49cfd5f0b6339524026ae68402
tanvirskyped <tnvr1990@gmail.com>
2018-07-02 16:54:30 +0600

    db username change commit

--- server/database/DB.js
+++ server/database/DB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://<tanvir_blessed>:<Naz1bAAA>@ds125031.mlab.com:25031/react-mern-db'
+    DB: 'mongodb://tanvir_blessed:Naz1bAAA@ds125031.mlab.com:25031/react-mern-db'
 }
